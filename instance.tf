5837bbb8f6e19de7f99d9a3e46dc675ec83b6497
Matthew Shiroma <19522250+maishiroma@users.noreply.github.com>
2023-05-21 18:37:23 -0700

    Made instance part of pool

--- instance.tf
+++ instance.tf
@@ -1,69 +1,94 @@
-resource "oci_core_instance" "self" {
-  display_name = local.instance_name
+resource "oci_core_instance_configuration" "self" {
+  compartment_id = var.compartment_id
+  display_name   = local.instance_name
 
-  shape = var.vm_shape
-  shape_config {
-    memory_in_gbs = var.vm_specs["memory"]
-    ocpus         = var.vm_specs["cpus"]
-  }
-  source_details {
-    source_id   = var.vm_image
-    source_type = "image"
-  }
+  instance_details {
+    instance_type = "compute"
 
-  compartment_id                      = var.compartment_id
-  availability_domain                 = var.availability_domain
-  is_pv_encryption_in_transit_enabled = "true"
-  availability_config {
-    recovery_action = "RESTORE_INSTANCE"
-  }
-  instance_options {
-    are_legacy_imds_endpoints_disabled = "false"
-  }
+    launch_details {
+      availability_domain                 = var.availability_domain
+      compartment_id                      = var.compartment_id
+      is_pv_encryption_in_transit_enabled = true
+      metadata = {
+        "ssh_authorized_keys" = var.pub_key
+      }
+      shape = var.vm_shape
 
-  create_vnic_details {
-    assign_private_dns_record = "true"
-    assign_public_ip          = "true"
-    nsg_ids                   = [oci_core_network_security_group.self[0].id]
-    subnet_id                 = var.existing_pub_subnet == "" ? oci_core_subnet.public[0].id : var.existing_pub_subnet
+      agent_config {
+        are_all_plugins_disabled = false
+        is_management_disabled   = false
+        is_monitoring_disabled   = false
+
+        plugins_config {
+          desired_state = "DISABLED"
+          name          = "Vulnerability Scanning"
+        }
+        plugins_config {
+          desired_state = "DISABLED"
+          name          = "Oracle Java Management Service"
+        }
+        plugins_config {
+          desired_state = "ENABLED"
+          name          = "OS Management Service Agent"
+        }
+        plugins_config {
+          desired_state = "ENABLED"
+          name          = "Compute Instance Run Command"
+        }
+        plugins_config {
+          desired_state = "ENABLED"
+          name          = "Compute Instance Monitoring"
+        }
+        plugins_config {
+          desired_state = "DISABLED"
+          name          = "Block Volume Management"
+        }
+        plugins_config {
+          desired_state = "DISABLED"
+          name          = "Bastion"
+        }
+      }
+
+      availability_config {
+        recovery_action = "RESTORE_INSTANCE"
+      }
+
+      create_vnic_details {
+        assign_private_dns_record = true
+        assign_public_ip          = true
+        defined_tags              = {}
+        freeform_tags             = {}
+        nsg_ids                   = []
+        skip_source_dest_check    = false
+        subnet_id                 = var.existing_pub_subnet == "" ? oci_core_subnet.public[0].id : var.existing_pub_subnet
+      }
+
+      instance_options {
+        are_legacy_imds_endpoints_disabled = false
+      }
+
+      shape_config {
+        memory_in_gbs = var.vm_specs["memory"]
+        ocpus         = var.vm_specs["cpus"]
+      }
+
+      source_details {
+        image_id    = var.vm_image
+        source_type = "image"
+      }
+    }
   }
+}
 
+resource "oci_core_instance_pool" "self" {
+  compartment_id = var.compartment_id
+  display_name   = var.project_name
+  size           = 1
 
-  agent_config {
-    is_management_disabled = "false"
-    is_monitoring_disabled = "false"
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Vulnerability Scanning"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Oracle Java Management Service"
-    }
-    plugins_config {
-      desired_state = "ENABLED"
-      name          = "OS Management Service Agent"
-    }
-    plugins_config {
-      desired_state = "ENABLED"
-      name          = "Compute Instance Run Command"
-    }
-    plugins_config {
-      desired_state = "ENABLED"
-      name          = "Compute Instance Monitoring"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Block Volume Management"
-    }
-    plugins_config {
-      desired_state = "DISABLED"
-      name          = "Bastion"
-    }
-  }
+  instance_configuration_id = oci_core_instance_configuration.self.id
 
-  metadata = {
-    ssh_authorized_keys = var.pub_key
-    # user_data           = base64encode(data.template_file.bootstrap.rendered)
+  placement_configurations {
+    availability_domain = var.availability_domain
+    primary_subnet_id   = var.existing_pub_subnet == "" ? oci_core_subnet.public[0].id : var.existing_pub_subnet
   }
 }
